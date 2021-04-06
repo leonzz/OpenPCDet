0f9a4c5edb9e0634a179401983be4738bb44e6e5
Leon Ziyang Zhang <leonzz@mikreal.com>
2021-04-05 23:08:08 -0400

    use flags for file names

--- tools/merge_predictions/evaluate_kitti_range.py
+++ tools/merge_predictions/evaluate_kitti_range.py
@@ -4,55 +4,70 @@ import pickle
 import numpy as np 
 import sys
 import os
+import argparse
 
 from pcdet.datasets.kitti.kitti_object_eval_python import eval as kitti_eval
 from merge_predictions import merge_results_all, merge_results_within_range
 
-PREDICTION_FULL_RANGE_FILE  = 'result_fr.pkl'
-PREDICTION_SHORT_RANGE_FILE = 'result_short.pkl'
-PREDICTION_LONG_RANGE_FILE  = 'result_long.pkl'
-
 GROUND_TRUTH_FILE = '../../data/kitti/kitti_infos_val.pkl'
 
+def parse_config():
+    parser = argparse.ArgumentParser(description='Evaluate kitti results based on saved prediction results.')
+    parser.add_argument('--full_range_result', type=str, default=None, help='The full range validation result pickle file.')
+    parser.add_argument('--short_range_result', type=str, default=None, help='The short range validation result pickle file.')
+    parser.add_argument('--long_range_result', type=str, default=None, help='The short range validation result pickle file.')
+    parser.add_argument('--merge_range_cutoff', type=float, default=34.0, help='The cutoff value to merge the short and long range result.')
+
+    args = parser.parse_args()
+    return args, parser
+
 def read_pickle_file(filename):
-	with open(filename, 'rb') as f:
-		content = pickle.load(f)
-		return content
+    with open(filename, 'rb') as f:
+        content = pickle.load(f)
+        return content
 
 def print_eval_result(ap_result_str, ap_dict):
-	# print(ap_result_str)
-	# print(ap_dict)
-	print('Easy: {:.2f}   Moderate: {:.2f}   Hard: {:.2f}\n'.format( ap_dict['Car_3d/easy_R40'], ap_dict['Car_3d/moderate_R40'], ap_dict['Car_3d/hard_R40'] ))
+    # print(ap_result_str)
+    # print(ap_dict)
+    print('Easy: {:.2f}   Moderate: {:.2f}   Hard: {:.2f}\n'.format( ap_dict['Car_3d/easy_R40'], ap_dict['Car_3d/moderate_R40'], ap_dict['Car_3d/hard_R40'] ))
 
 def main():
-	current_folder = os.path.abspath(os.path.dirname(__file__)) + '/'
+    args, parser = parse_config()
+    current_folder = os.path.abspath(os.path.dirname(__file__)) + '/'
 
-	groundtruth = read_pickle_file(os.path.join(current_folder,GROUND_TRUTH_FILE))
-	eval_gt_annos = [copy.deepcopy(info['annos']) for info in groundtruth]    
+    groundtruth = read_pickle_file(os.path.join(current_folder,GROUND_TRUTH_FILE))
+    eval_gt_annos = [copy.deepcopy(info['annos']) for info in groundtruth]
 
-	# Full range evaluation
-	full_range_result = read_pickle_file(os.path.join(current_folder,PREDICTION_FULL_RANGE_FILE))
-	ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, full_range_result, 'Car')
-	
-	print('\nModel trained with full range data:')
-	print_eval_result(ap_result_str, ap_dict)
+    show_help = True
 
-	# Evaluation of merged result from models handling separate ranges
-	print('\nMerged result from models handling separate ranges:')
-	short_range_result = read_pickle_file(os.path.join(current_folder,PREDICTION_SHORT_RANGE_FILE))
-	long_range_result = read_pickle_file(os.path.join(current_folder,PREDICTION_LONG_RANGE_FILE))
+    # Full range evaluation
+    if args.full_range_result is not None:
+        show_help = False
+        full_range_result = read_pickle_file(args.full_range_result)
+        ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, full_range_result, 'Car')
+    
+        print('\nModel trained with full range data:')
+        print_eval_result(ap_result_str, ap_dict)
 
-	merged_result = merge_results_all(short_range_result, long_range_result)
-	ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, merged_result, 'Car')
-	print('\n- Merge all:')
-	print_eval_result(ap_result_str, ap_dict)
+    # Evaluation of merged result from models handling separate ranges
+    if args.long_range_result is not None and args.short_range_result is not None:
+        show_help = False
+        print('\nMerged result from models handling separate ranges:')
+        short_range_result = read_pickle_file(args.short_range_result)
+        long_range_result = read_pickle_file(args.long_range_result)
 
-	#for range_cutoff in [30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0]:
-	for range_cutoff in [34.0]:
-		merged_result = merge_results_within_range(short_range_result, long_range_result, range_cutoff)
-		ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, merged_result, 'Car')
-		print('\n- Merge with range cutoff at {}:'.format(range_cutoff))
-		print_eval_result(ap_result_str, ap_dict)
+        merged_result = merge_results_all(short_range_result, long_range_result)
+        ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, merged_result, 'Car')
+        print('\n- Merge all:')
+        print_eval_result(ap_result_str, ap_dict)
+
+        merged_result = merge_results_within_range(short_range_result, long_range_result, args.merge_range_cutoff)
+        ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, merged_result, 'Car')
+        print('\n- Merge with range cutoff at {}:'.format(args.merge_range_cutoff))
+        print_eval_result(ap_result_str, ap_dict)
+
+    if show_help:
+        parser.print_help()
 
 if __name__ == '__main__':
-	main()
+    main()
